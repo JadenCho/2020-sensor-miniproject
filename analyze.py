943eae47cce21ae1bebc4783a832d357cecc55b4
JadenCho <34437519+JadenCho@users.noreply.github.com>
2020-09-14 17:17:13 -0600

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -24,7 +24,7 @@ def load_data(file: Path) -> T.Dict[str, pandas.DataFrame]:
     occupancy = {}
     co2 = {}
 
-    with open("data.txt", "r") as f:
+    with open(file, "r") as f:
         for line in f:
             r = json.loads(line)
             room = list(r.keys())[0]
