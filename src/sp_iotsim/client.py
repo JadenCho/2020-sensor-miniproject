ee87050f7095aa502f0819895601eeb5a9bfadeb
JadenCho <34437519+JadenCho@users.noreply.github.com>
2020-09-14 21:02:37 -0600

    Update client.py

--- src/sp_iotsim/client.py
+++ src/sp_iotsim/client.py
@@ -150,17 +150,17 @@ def cli():
         
         
         plt.figure(1)
-        plt.hist(x1, bins = 1)
+        plt.hist(x1, bins = 1, orientation="horizontal")
         plt.ylabel("Temperature")
         plt.title("Temperature Histogram")
         
         plt.figure(2)
-        plt.hist(x2, bins = 1)
+        plt.hist(x2, bins = 1, orientation="horizontal")
         plt.ylabel("Occupancy")
         plt.title("Occupancy Histogram")
         
         plt.figure(3)
-        plt.hist(x3, bins = 1)
+        plt.hist(x3, bins = 1, orientation="horizontal")
         plt.ylabel("CO2")
         plt.title("CO2 Histogram")
         plt.show()
