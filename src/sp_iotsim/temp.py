a2bf4b3dc6291244d1196140c6b08da66729721d
JadenCho <34437519+JadenCho@users.noreply.github.com>
2020-09-15 19:06:27 -0600

    Update temp.py

--- src/sp_iotsim/temp.py
+++ src/sp_iotsim/temp.py
@@ -1,6 +1,8 @@
 import argparse
 from sp_iotsim.fileio import load_data
 import pandas as pd
+import numpy as np
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filepath', help='file read in to analyze')
