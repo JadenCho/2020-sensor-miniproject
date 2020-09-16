55f713fa5d24dba2afd55574a59cef5163c87b34
mloebs <70725808+mloebs@users.noreply.github.com>
2020-09-15 20:59:01 -0400

    fix file -> filepath

--- src/sp_iotsim/temp.py
+++ src/sp_iotsim/temp.py
@@ -4,6 +4,6 @@ from sp_iotsim.fileio import load_data
 parser = argparse.ArgumentParser()
 parser.add_argument('filepath', help='file read in to analyze')
 args = parser.parse_args()
-file = args.filepath
+filepath = args.filepath
 
-data = load_data(file)
+data = load_data(filepath)
