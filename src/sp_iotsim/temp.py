8c796fb5891ef1498f55cbafa4379733ed4f6329
JadenCho <34437519+JadenCho@users.noreply.github.com>
2020-09-15 19:05:33 -0600

    Update temp.py

--- src/sp_iotsim/temp.py
+++ src/sp_iotsim/temp.py
@@ -1,5 +1,6 @@
 import argparse
 from sp_iotsim.fileio import load_data
+import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filepath', help='file read in to analyze')
@@ -7,4 +8,14 @@ args = parser.parse_args()
 filepath = args.filepath
 
 data = load_data(filepath)
-print(data)
+#print(data)
+#temperature = {}
+#with open(data, "r") as stuff:
+#    for line in stuff:
+#        r = json.loads(line)
+#        roomkeys = list(r.keys())[0]
+#        time = datetime.fromisoformat(r[roomkeys]["time"])
+#                
+#        temperature[time] = {roomkeys: r[roomkeys]["temperature"][0]}
+
+temp = pd.DataFrame.from_dict(data["temperature"], "index").sort_index()
